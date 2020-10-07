5e98e435ffdad5602fda87dca3327ea1fa2e4a97
josetv91 <61167951+josetv91@users.noreply.github.com>
2020-10-06 22:24:56 -0500

    Update Trimclip.py

--- Trimclip.py
+++ Trimclip.py
@@ -1,7 +1,11 @@
 from moviepy.editor import VideoFileClip
 from moviepy.editor import *
 import os
+
+# Folder containing the videos, with no matter if they're located in subdirectories
 generalpath ="/home/lx/Desktop/videos"
+
+# Creating the name, path and full name (path+name) of each video & storing in a arrays
 listnames=[]
 listpath=[]
 fullpath=[]
@@ -12,9 +16,9 @@ for roots, divs, files in os.walk(generalpath):
          listpath.append(os.path.join(roots))
          fullpath.append(os.path.join(roots,file))
 
-print(listnames)
-print(listpath)
-
+# print(listnames)
+# print(listpath)
+# Editing the video, trim the 9 first seconds of every video and store them appending the string 'a' on every video's name, every video is created inside the folder of the original video
 for i in range(len(listnames)):
    clip = VideoFileClip(fullpath[i])
    newclip = clip.subclip(9.25,clip.duration)
